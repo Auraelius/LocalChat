e44816afd6df8646a6cc5dfb2e3d0125323c59b5
Al Zimmerman <auraelius@gmail.com>
2014-11-20 19:12:37 -0800

    Initial Commit

---
+++ LocalChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalChat
+//
+//  Created by Al Zimmerman on 11/20/14.
+//  Copyright (c) 2014 Al Zimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LocalChat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LocalChat
+//
+//  Created by Al Zimmerman on 11/20/14.
+//  Copyright (c) 2014 Al Zimmerman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
