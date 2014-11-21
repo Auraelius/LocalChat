c5449b000af37d6c5916e1b67876cad97799ad8f
Al Zimmerman <auraelius@gmail.com>
2014-11-20 21:12:59 -0800

    End of first class. started work on textfield

--- LocalChat/ViewController.m
+++ LocalChat/ViewController.m
@@ -17,7 +17,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    // subscribe to global notifications about when the keyboard shows and hides
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWasShown:)
+                                                 name:UIKeyboardDidShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillHide:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+    
 }
+
+// now the methods to handle the keyboard notifications
+
+-(void)keyboardWasShown:(NSNotification *)notification {
+    // animate the textfield to be above the keyboard
+}
+
+-(void)keyboardWillHide:(NSNotification *)notification {
+    // animate the textfield to be back at the bottom of the screen
+}
+
+
+-(void) hideKeyboard {
+    [self.chatText resignFirstResponder]; // hides the keyboard when revealed by focusing on the text field. 'self' is the view conroller
+}
+
+
+-(void)textFieldDidEndEditing:(UITextField *)textField {
+
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField { // when user clicks RETURN
+    [self hideKeyboard]; // call the method above 
+    return NO; // don't add the CR to the string, just react to it
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
